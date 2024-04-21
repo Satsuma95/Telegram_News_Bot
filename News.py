ca78731f647cabd8240044a2fa090b216fa8868c
Satsuma95 <drochila444@gmail.com>
2024-04-21 16:59:19 +0300

    osnova

--- News.py
+++ News.py
@@ -18,18 +18,31 @@ def start(message):
 @AXTUNG.message_handler(commands=['News'])
 def news(message):
     user_mes = message.text.split("/News")[1].strip()
-    api(user_mes)
+    ret = api(user_mes)
+    if len(ret)>0:
+        AXTUNG.send_message(message.chat.id, text=ret)
+    else:
+        print ("404")
+
+
 
 def api(user_mes):
     url = f"https://newsapi.org/v2/everything?q={user_mes}&language=ru&apiKey={API}"
     request_result = requests.get(url)
     JS = request_result.json()
     al = JS["articles"]
+    finmes = ""
     for x in al:
-        print(x["description"],x["title"],x["url"])
-        break
+        mes = x["description"],x["title"],x["url"]
+        sum_one_news = len(mes[0]) + len(mes[1]) + len(mes[2])
+        if len(finmes)+sum_one_news<4096:
+            finmes += (f"{mes[1]}\n{mes[0]}\n{mes[2]}\n\n")
+        else:
+            print ("404 2")
 
-api("Майнкрафт")
+
+    return finmes
+
 
 @AXTUNG.message_handler(content_types=['text'])
 def text(message):
@@ -40,8 +53,12 @@ AXTUNG.polling()
 
 
 """
-ОЧЕНЬ МНОГО ДОМАШКИ!!!!! ОСТОРОЖНО!!
+сделать функционал, по которому можно будет отправлять сообщение пользователю
+смысл такой:
+/send 34873847 привет, как дела? -  эту строчку разделить на команду сенд, айдишник и сам текст
+после чего просто отправить сообщение челу по айдишнику. если всё будет ок, то сделать тоже самое в обратную сторону
+/send_adm привет, го в сэтисфектори??!!?!?!
+и всё сообщение должно улететь тебе :)
 
-1. Юзер вводит: "/news роблокс", нужно отловить всё то, что он написал после самой команды и поместить в переменную
-2. Данные отправить по ссылке точно также, как и мы делали в функции locatio или get_city_image в прошлом проекте
-3. Получить всё с сайта и в красивом виде отправить пользователюы"""
+
+"""
